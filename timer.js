86f52017b8073c528e91ffd404291cb2a707c255
Denis Trailin <hiyou102@gmail.com>
2017-10-01 17:04:10 -0700

    Bug fixes (#8)

--- timer.js
+++ timer.js
@@ -3,6 +3,7 @@ const firebase = require('./firebase.js')
 var schedule = require('node-schedule');
 var send = require('./sendingHelpers.js');
 var quiz = require('./quiz.js')
+var jobMap = {}
 
 firebase.getAllJobs().then((jobs) => {
   if(jobs != null){
@@ -22,8 +23,10 @@ function scheduleQuizJob(userId, hour, minute){
   var rule = new schedule.RecurrenceRule();
   rule.minute = minute;
   rule.hour
-  
-  schedule.scheduleJob(rule,() => {
+  if(userId in jobMap){
+    jobMap[userId].cancel()
+  }
+  jobMap[userId] = schedule.scheduleJob(rule,() => {
     send.sendTextMessage(userId, "Hey, it's time for your daily quiz!");
     quiz.beginQuiz(userId);
     console.log('schedulingJob');
